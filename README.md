e8cd1c55300afe02884a05049370eeaa784e57f2
Stéphane Lopes <stephane.lopes@uvsq.fr>
2016-10-06 17:11:02 +0200

    termine l'implémentation de la pile

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # stack-demo
 A C project for a stack
+
+## Compilation
+```
+gcc -o stack-demo -Wall -g main.c
+```
+
+## Valgrind
+```
+valgrind ./stack-demo
+```
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+typedef int value_t;
 
 typedef struct element {
-    int value;
+    value_t value;
     struct element *suivant;
 } element_t;
 
@@ -12,11 +15,30 @@ void stack_create(stack_t *stack) {
     *stack = NULL;
 }
 
-void stack_push(stack_t *stack, int top_value) {
+void stack_delete(stack_t *stack) {
+    if (*stack) {
+        stack_t to_free = *stack;
+        *stack = (*stack)->suivant;
+        free(to_free);
+        stack_delete(stack);
+    }
+}
+
+void stack_push(stack_t *stack, value_t top_value) {
     stack_t top = malloc(sizeof(element_t));
     top->value = top_value;
     top->suivant = *stack;
     *stack = top;
+}
+
+value_t stack_pop(stack_t *stack) {
+    if (*stack == NULL)
+        return INT_MAX;
+    stack_t old_top = *stack;
+    *stack = (*stack)->suivant;
+    value_t value = old_top->value;
+    free(old_top);
+    return value;
 }
 
 void stack_print(stack_t stack) {
@@ -37,6 +59,18 @@ int main() {
         stack_push(&stack, i);
     }
     stack_print(stack);
+
+    for (int i = 0; i < 11; ++i) {
+        printf("%d | ", stack_pop(&stack));
+        stack_print(stack);
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        stack_push(&stack, i);
+    }
+    stack_print(stack);
+
+    stack_delete(&stack);
 
     return 0;
 }
